f29373057ab0d25f1e2e49de153aa038fcbc2378
Embla Mathea <emblamathea@outlook.com>
2022-02-15 15:26:59 +0100

    RENAME PLANE CLASS
    Is always 3D, redundant naming

--- code/as03/Objects/Plane.cs
+++ code/as03/Objects/Plane.cs
@@ -1,12 +1,12 @@
 ﻿namespace as03.Objects;
 
-public class Plane3D
+public class Plane
 {
     public Vector3D normal;
     public double d;
     public Vector3D[] points;
 
-    public Plane3D(Vector3D normal, double d)
+    public Plane(Vector3D normal, double d)
     {
         this.normal = normal;
         this.d = d;
