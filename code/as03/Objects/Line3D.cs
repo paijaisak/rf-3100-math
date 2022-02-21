1c21213833e9ee931f04e69cb60d9fd894833520
Embla Mathea <emblamathea@outlook.com>
2022-02-21 22:05:41 +0100

    ADD LINE3D CLASS WITH TESTS

---
+++ code/as03/Objects/Line3D.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+using System.Numerics;
+using System.Runtime.Intrinsics;
+
+namespace as03.Objects;
+
+public class Line3D
+{
+    // Point
+    public Vector3D P { get; }
+
+    // Direction
+    public Vector3D V { get; }
+
+    public Line3D(Vector3D point, Vector3D direction)
+    {
+        P = point;
+        V = direction;
+    }
+
+    public bool Intersects(Line3D line, out Point3D? p)
+    {
+        var p1 = P;
+        var p2 = line.P;
+
+        var v1 = V;
+        var v2 = line.V;
+
+        var t = ( ((p2 - p1).Cross(v2)) * (v1.Cross(v2)) ) 
+            / ( ((v1.Cross(v2)).Length) * ((v1.Cross(v2)).Length) );
+
+        var s = ( ((p1 - p2).Cross(v1)) * (v2.Cross(v1)) ) 
+                / ( ((v2.Cross(v1)).Length) * ((v2.Cross(v1)).Length) );
+
+        var res = Math.Abs(t - s) < 0.000001 &&
+                  !AreParallel(this, line);
+        if (res)
+        {
+            p = (Point3D) (p1 + (t * v1));
+        }
+
+        else if (double.IsNaN(t) || double.IsNaN(s))
+        {
+            p = (Point3D) (p1 + (t * v1));
+        }
+
+        else
+        {
+            if (AreParallel(this, line))
+            {
+                Debug.WriteLine("\nLines are parallel\n");
+            }
+
+            p = null;
+        }
+
+        return res;
+    }
+
+    public static bool AreParallel(Line3D l, Line3D m)
+    {
+        return l.V.Cross(m.V) == new Vector3D(0, 0, 0);
+    }
+}
---
+++ code/math-tests/LineTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using as03.Objects;
+using NUnit.Framework;
+
+namespace math_tests;
+
+[TestFixture]
+public class LineTests
+{
+    [Test]
+    public void TestIntersections()
+    {
+        // FIRST TWO SHOULD NOT INTERSECT, THEY ARE PARALLEL
+        var f = new Line3D(new Vector3D(0, 0, 0), new Vector3D(2, 4, 6));
+        var g = new Line3D(new Vector3D(2, 6, 10), new Vector3D(1, 2, 3));
+
+        var h = new Line3D(new Vector3D(-1, -2, -3),  new Vector3D(6, 2, 4));
+
+        var res = f.Intersects(g, out var point);
+
+        if (!res)
+        {
+            Console.WriteLine(point);
+        }
+
+        Assert.IsFalse(res);
+
+        res = f.Intersects(h, out point);
+
+        Assert.IsTrue(res);
+
+        if (res)
+        {
+            Console.WriteLine("Lines intersect at: " + point);
+        }
+    }
+
+    [Test]
+    public void TestParallelLines()
+    {
+        var r = new Line3D(new Vector3D(0, 0, 0), new Vector3D(2, 4, 6));
+        var s = new Line3D(new Vector3D(2, 6, 10), new Vector3D(1, 2, 3));
+
+        var q = new Line3D(new Vector3D(6, 12, 1),  new Vector3D(6, 2, 4));
+
+        Assert.IsTrue(Line3D.AreParallel(r, s));
+        Assert.IsFalse(Line3D.AreParallel(r, q));
+    }
+}
